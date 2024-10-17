bc0cbf734ac9f48a01b66b20557628d854269285
Haikal Maghsin <haikal.maghsin0756@gmail.com>
2024-10-17 10:32:35 +0700

    mengganti hovernya

--- resources/views/components/nav-dropdown.blade.php
+++ resources/views/components/nav-dropdown.blade.php
@@ -2,7 +2,7 @@
 
 <div class="relative inline-block text-left">
     {{-- Trigger (button for dropdown) --}}
-    <button @click="open = ! open" class="inline-flex items-center px-4 py-3 text-sm font-medium leading-5 text-gray-500 hover:text-white hover:bg-gray-100 focus:outline-none focus:text-gray-700 focus:bg-gray-100 transition duration-150 ease-in-out">
+    <button @click="open = ! open" class="inline-flex items-center px-4 py-3 text-sm font-medium leading-5 text-gray-500 hover:text-white hover:text-underlinehover:bg-gray-100 focus:outline-none focus:text-gray-700 focus:bg-gray-100 transition duration-150 ease-in-out">
         {{ __('Genre') }}
         <svg class="ml-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
