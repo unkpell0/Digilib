335e372d33c19db5795fd3d658cf13952ffeb93b
unkpell0 <112400860+unkpell0@users.noreply.github.com>
2024-10-26 23:09:04 +0700

    update login and register fix #1
    * login
    
    * update menu dan navigation
    
    * update login dan register
    
    * update login dan register
    
    * login register fix #1

--- resources/views/components/authentication-card.blade.php
+++ resources/views/components/authentication-card.blade.php
@@ -1,9 +1,9 @@
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+<div class="min-h-screen flex flex-col lg:w-3/5 w-full sm:justify-center items-center pt-4 sm:pt-0 bg-gray-100 overflow-y-scroll">
     <div>
         {{ $logo }}
     </div>
 
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+    <div class="w-full sm:max-w-md mx-2 my-4 px-6 py-3 text-base bg-white shadow-md overflow-hidden sm:rounded-lg">
         {{ $slot }}
     </div>
 </div>
--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -100,9 +100,9 @@
                             </x-dropdown-link>
                             <form method="POST" action="{{ route('logout') }}">
                                 @csrf
-                                <x-dropdown-link href="{{ route('logout') }}" @click.prevent="$root.submit();">
+                                <x-dropdown-link href="{{ route('logout') }}" onclick="event.preventDefault(); this.closest('form').submit();">
                                     {{ __('Log Out') }}
-                                </x-dropdown-link>
+                                </x-dropdown-link>                                
                             </form>
                         </x-slot>
                     </x-dropdown>
